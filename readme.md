1d13dc2d11ca8e41665407b01ae2fd99274dd14f
Dhruv Mehta <meht0031@algonquinlive.com>
2020-01-13 17:02:51 -0500

    Second Commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+# git javascript
+## Dhruv Mehta
--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let instructorName = 'Dhruv'
+let name= 'Dhruv'
+console.log(`Hello, my name is ${name}.`)
 
-instructorName = 'Simran'
+let number = 5
+console.log(`the number ${number} multiplied bu 2 is ${number * 2}.`)
+
+console.log(`The number multiplied by 5 is ${product * 5}.`)
+console.log(`The result multiplied by 5 is ${product * 5}.`)
+console.log(`The result divided by 5 is ${(product * 5) / number}.`)
+let finaValue = (product * 5) / number
+console.log(`The result minus 7 is ${finaValue - 7}.`)
 
 
 
-console.log(instructorName)
